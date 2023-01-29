ea48c6ff5eb1b09a06b60f45005233039bfa7844
Tartsi <tarvo.lilja@hotmail.fi>
2023-01-30 01:03:29 +0200

    Added template register-page, db-logic and user addition functionality, fix SQL-error in tables.sql

---
+++ db_logic.py
@@ -0,0 +1,19 @@
+from db import db
+from werkzeug.security import check_password_hash, generate_password_hash
+from app import app
+
+
+def add_user(username, password, admin):
+
+    try:
+
+        sql = "INSERT INTO users (username, password, admin) VALUES (:username, :password, :admin)"
+
+        db.session.execute(sql, {
+                           "username": username, "password": generate_password_hash(password), "admin": admin})
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        db.session.rollback()
+        return False
--- routes.py
+++ routes.py
@@ -8,6 +8,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
