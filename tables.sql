8b8a27a7b3b9fead21e95c2e9d83fc8ed8524bd3
Tartsi <tarvo.lilja@hotmail.fi>
2023-02-14 02:16:07 +0200

    Modified Database tables

--- tables.sql
+++ tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(40) UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     admin INTEGER NOT NULL DEFAULT 0
 );
@@ -8,35 +8,28 @@ CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
+    price INTEGER NOT NULL,
     amount INTEGER NOT NULL DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS shopping_cart (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL DEFAULT 0,
-    total_price DECIMAL NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users,
-    FOREIGN KEY (product_id) REFERENCES products
+    user_id INTEGER REFERENCES users,
+    product_id INTEGER REFERENCES products,
+    quantity INTEGER NOT NULL DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS completed_orders (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
-    date_created TIMESTAMP DEFAULT now(),
+    user_id INTEGER REFERENCES users,
+    cart_id INTEGER REFERENCES shopping_cart,
     address TEXT NOT NULL,
     total_price DECIMAL NOT NULL DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users,
-    FOREIGN KEY (product_id) REFERENCES products
+    date_created TIMESTAMP DEFAULT now()
 );
 CREATE TABLE IF NOT EXISTS reviews (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
+    user_id INTEGER REFERENCES users,
+    product_id INTEGER REFERENCES products,
     rating INTEGER NOT NULL DEFAULT 0,
     description TEXT NOT NULL,
-    date_created TIMESTAMP DEFAULT now(),
-    FOREIGN KEY (user_id) REFERENCES users,
-    FOREIGN KEY (product_id) REFERENCES products
+    date_created TIMESTAMP DEFAULT now()
 );
